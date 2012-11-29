c94bf6087008c8d4e46cd41e97359a9ace2238b3
Joonha Shin <shin.joonha@gmail.com>
2012-11-29 18:54:28 -0500

    Adds a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bowling::Application.config.secret_token = 'af374b26e6b65afc39e255ffc22581a737588819432342c6de0f31a1263770522044ce7d4cfe21c53a030d217b2738399f239f10974c5ebafc31d13ad0c8d885'
